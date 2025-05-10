877263fa02f144b9b41401645adf7eb534c5934a
Daniel Gnitetskiy <dgnitetskiy@gmail.com>
2025-05-10 22:33:36 +0300

    fix

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-// связь с бд
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql://{settings.DB_USER}:{settings.DB_PASSWD}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
