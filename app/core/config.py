65a575a5c1c701f3e93ef56c561a4781188e123e
Daniel Gnitetskiy <dgnitetskiy@gmail.com>
2025-05-10 19:45:46 +0300

    env vars

--- app/core/config.py
+++ app/core/config.py
@@ -3,21 +3,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "My API"
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "default-key")
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
-    DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: int = int(os.getenv("DB_PORT", 5432))
-    DB_NAME: str = os.getenv("DB_NAME", "mydb")
+    PROJECT_NAME: str = "Fable"
+    SECRET_KEY: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     ALGORITHM: str = "HS256"
     API_V1_STR: str = "/api/v1"
 
-    # Pydantic v2 style config
     model_config = SettingsConfigDict(
-        extra="ignore",   # silently ignore unknown env vars
+        env_file=None,
+        extra="ignore",
     )
-
 
 settings = Settings()
