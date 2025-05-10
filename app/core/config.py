07196b227e00faa0cdd34271b926e8d1c5cb6f09
Daniel Gnitetskiy <dgnitetskiy@gmail.com>
2025-05-10 19:32:07 +0300

    annotations in pydamin v2

--- app/core/config.py
+++ app/core/config.py
@@ -4,12 +4,12 @@ import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "My API"
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
-    DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = int(os.getenv("DB_PORT", 5432))
-    DB_NAME = os.getenv("DB_NAME", "mydb")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "default-key")
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: int = int(os.getenv("DB_PORT", 5432))
+    DB_NAME: str = os.getenv("DB_NAME", "mydb")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     ALGORITHM: str = "HS256"
     API_V1_STR: str = "/api/v1"
